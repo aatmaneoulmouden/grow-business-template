aaf1fe6519c4ebc6cd2df4b553f62c5a6ff83fc9
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-02-19 10:46:09 +0100

    Active link when scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,40 @@ menuLinks.forEach(activeLink => {
         activeLink.classList.add('active');
     });
 });
+
+// Active menus when scrolling
+window.addEventListener('scroll', () => {
+    const homeOffsetTop = document.querySelector('#home').offsetTop;
+    const aboutOffsetTop = document.querySelector('#about').offsetTop;
+    const servicesOffsetTop = document.querySelector('#services').offsetTop;
+    const blogOffsetTop = document.querySelector('#blog').offsetTop;
+    const contactOffsetTop = document.querySelector('#contact').offsetTop;
+    var scrollY = window.scrollY;
+
+    if (scrollY >= homeOffsetTop && scrollY < aboutOffsetTop) {
+        menuLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        document.querySelector('#home-link').classList.add('active');
+    } else if (window.scrollY >= aboutOffsetTop && scrollY < servicesOffsetTop) {
+        menuLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        document.querySelector('#about-link').classList.add('active');
+    } else if (window.scrollY >= servicesOffsetTop && scrollY < blogOffsetTop) {
+        menuLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        document.querySelector('#services-link').classList.add('active');
+    } else if (window.scrollY >= blogOffsetTop && scrollY < contactOffsetTop) {
+        menuLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        document.querySelector('#blog-link').classList.add('active');
+    } else if (window.scrollY >= contactOffsetTop) {
+        menuLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        document.querySelector('#contact-link').classList.add('active');
+    }
+});
